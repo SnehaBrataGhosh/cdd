66a3bb68f9811834790b0eecb287f9430c32fdf1
SnehaBrataGhosh <2300033769cser@gmail.com>
2026-01-02 15:21:55 +0530

    Add files via upload

---
+++ Addd.java
@@ -0,0 +1,10 @@
+package com.JeetGhosh;
+
+public class Addd {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 7;
+        int sum = num1 + num2;
+        System.out.println("Sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
